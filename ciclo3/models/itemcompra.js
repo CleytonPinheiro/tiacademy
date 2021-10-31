c4aaf3216c21ccd85ebb9b060948f329acc0a2c2
CleytonPinheiro <cleyton_pin@hotmail.com>
2021-10-31 20:47:47 -0300

    cria migrate e models e configura os relacionamentos

---
+++ ciclo3/models/itemcompra.js
@@ -0,0 +1,32 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class ItemCompra extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      ItemCompra.belongsTo(models.Compra, {
+        foreignKey: "CompraId",
+        as: "compra",
+      });
+      ItemCompra.belongsTo(models.Produto, {
+        foreignKey: "ProdutoId",
+        as: "produto",
+      });
+    }
+  }
+  ItemCompra.init(
+    {
+      quantidade: DataTypes.INTEGER,
+      valor: DataTypes.FLOAT,
+    },
+    {
+      sequelize,
+      modelName: "ItemCompra",
+    }
+  );
+  return ItemCompra;
+};
